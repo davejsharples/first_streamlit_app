3fce8150b60996d7b4f96e5f4ad1e1322647e053
davejsharples <114679222+davejsharples@users.noreply.github.com>
2022-10-03 09:53:49 +0100

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents Not New Healthy Diner')
+
+streamlit.header ('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamit.text ('Hard-boiled Free-Range Egg')
